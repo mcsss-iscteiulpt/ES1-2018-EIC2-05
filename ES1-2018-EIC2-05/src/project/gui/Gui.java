c74ff51e9b40b7598dff771019f372adf3145fae
mcsss <mcsss@iscte-iul.pt>
2018-10-21 02:16:48 +0100

    gui 0.01

--- ES1-2018-EIC2-05/src/project/gui/Gui.java
+++ ES1-2018-EIC2-05/src/project/gui/Gui.java
@@ -1,13 +1,46 @@
 package project.gui;
 
-import java.awt.Frame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
 
 public class Gui {
-	
-	private Frame frame;
-	
-	public Gui()	{
-		
+
+	private JFrame frame;
+
+	public Gui(String frameTitle) {
+		frame = new JFrame(frameTitle);
+		frame.setSize(900, 600);
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+		int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+		frame.setLocation(x, y);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		addContent();
 	}
-	
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void addContent() {
+		frame.setLayout(new BorderLayout());
+		JLabel bda=new JLabel(new ImageIcon("Imgs/BDALogo.png"));
+		frame.add(bda,BorderLayout.NORTH);
+	  
+	    
+
+	}
+
+	public void open() {
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Gui gui = new Gui("BDA(BOM DIA ACADEMIA)");
+		gui.addContent();
+		gui.open();
+	}
 }
