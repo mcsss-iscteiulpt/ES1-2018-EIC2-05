826715ad258fc18138bbeff183245457c9c8ae68
VascoBranco <vjsla@iscte-iul.pt>
2018-11-22 18:16:16 +0000

    Foi adicionada nova JARlib. Jsoup.

--- ES1-2018-EIC2-05/src/project/mail/Receive.java
+++ ES1-2018-EIC2-05/src/project/mail/Receive.java
@@ -1,8 +1,14 @@
 package project.mail;
 
 
+
 import java.util.Properties;
 import javax.mail.*;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.internet.MimeMultipart;
+import org.jsoup.Jsoup;
+
 
 
 
@@ -26,6 +32,12 @@ public class Receive {
 				System.out.println("Subject: " + message.getSubject());
 				System.out.println("From: " + message.getFrom()[0]);
 				System.out.println("Sent Date: " + message.getSentDate());
+				try {
+					System.out.println("Content: " + getTextFromMessage(message) );
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			//closing email folder
 			emailFolder.close(false);
@@ -38,6 +50,32 @@ public class Receive {
 			e.printStackTrace();
 		}
 	}
+	
+	private static String getTextFromMessage(Message message) throws Exception {
+	    if (message.isMimeType("text/plain")){
+	        return message.getContent().toString();
+	    }else if (message.isMimeType("multipart/*")) {
+	        String result = "";
+	        MimeMultipart mimeMultipart = (MimeMultipart)message.getContent();
+	        int count = mimeMultipart.getCount();
+	        for (int i = 0; i < count; i ++){
+	            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+	            if (bodyPart.isMimeType("text/plain")){
+	                result = result + "\n" + bodyPart.getContent();
+	                break;  //without break same text appears twice in my tests
+	            } else if (bodyPart.isMimeType("text/html")){
+	                String html = (String) bodyPart.getContent();
+	                result = result + "\n" + Jsoup.parse(html).text();
+
+	            }
+	        }
+	        
+	        return result;
+	    }
+	    return "";
+	}
+	
+	
 	public static void main(String[] args)
 	{
 		receiveMail("es1.eic2.5@gmail.com","MiguelNeto15");
